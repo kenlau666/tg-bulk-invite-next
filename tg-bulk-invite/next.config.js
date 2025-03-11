7c875547ca706b6e4434192bce49eb09f2eca4e1
kenlau666 <kenlauhong2015@gmail.com>
2025-03-11 12:21:34 +0800

    fixed: domain name

--- tg-bulk-invite/next.config.js
+++ tg-bulk-invite/next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:5328/api/:path*"
-            : "http://tg.surftunnel88.com/api/:path*",
+            : "http://34.92.136.54:5328/api/:path*",
       },
     ];
   },
